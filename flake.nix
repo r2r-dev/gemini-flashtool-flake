40e4c5332c856e2e5d9093eeb491c99c53eafadd
r2r-dev <dev@r2r.sh>
2021-07-18 16:02:21 +0200

    fix issues with gtk and fontconfig

--- flake.nix
+++ flake.nix
@@ -7,24 +7,37 @@
       flake = false;
     };
     flokli = {
-      url = "github:flokli/nix-packages?rev=c86185d10ff8456110dcfa798497ea9336f56ad0";
+      url =
+        "github:flokli/nix-packages?rev=c86185d10ff8456110dcfa798497ea9336f56ad0";
       flake = false;
     };
   };
 
   outputs = { self, nixpkgs, flokli }:
-      let
-       
-        pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ (import "${flokli}") ];};
+    let
 
-      in {
-        packages.x86_64-linux.defaultPackage.x86_64-linux = pkgs.sp-flash-tool;
-
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ sp-flash-tool ];
-          inputsFrom = builtins.attrValues self.packages;
-        };
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ (import "${flokli}") ];
       };
-}
+      fontsConf = pkgs.makeFontsConf {
+        fontDirectories = [ pkgs.fira-code-symbols pkgs.fira-code ];
+      };
 
+    in {
+      packages.x86_64-linux.defaultPackage.x86_64-linux =
+        pkgs.sp-flash-tool.overrideAttrs (oldAttrs: rec {
+          installPhase = oldAttrs.installPhase + ''
+            wrapProgram $out/bin/flash_tool \
+              --set FONTCONFIG_FILE ${fontsConf} \
+              --set GDK_PIXBUF_MODULE_FILE "${pkgs.librsvg}/${pkgs.gdk_pixbuf.moduleDir}.cache"
+          '';
+        });
+
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [ sp-flash-tool ];
+        inputsFrom = builtins.attrValues self.packages;
+      };
+    };
+}
 
